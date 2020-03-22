49f4d329425bca8f4bdedd770e26ab67908ef94e
David <davidjamescottrell@gmail.com>
2020-03-22 11:50:51 +0000

    COVID-19 Update

--- src/App.js
+++ src/App.js
@@ -43,7 +43,6 @@ class App extends Component {
         require('dotenv').config()
         
         smoothscroll.polyfill(); //Initiates smooth scroll functionality
-
     }
 
     showAnnouncement = (msg) => {
@@ -55,15 +54,14 @@ class App extends Component {
     }
 
     componentDidMount(){
-        //"Unfortunately due to the current situation regarding COVID-19 we have decided to close. We aim to re-open as soon as the situation resolves. Thank you."
-        this.showAnnouncement("COVID-19: Wribbenhall School is an alternative provision for Special Needs Education, following Government advice we currently remain open.");
+        this.showAnnouncement("COVID-19: Wribbenhall School is an alternative provision for Special Needs Education, following Government advice we currently remain open. Please observe social distance.");
     }
 
     render() {
         // [["Page name", "URL"]]
         const pageNames = [
-            ["Home", "/"], 
-            ["Contact Us", "/contact"], 
+            ["Home", "/"],
+            ["Contact Us", "/contact"],
             ["Our Policies", "/policies"],
             ["Gallery", "/gallery"],
             ["Calendar", "/calendar"]
