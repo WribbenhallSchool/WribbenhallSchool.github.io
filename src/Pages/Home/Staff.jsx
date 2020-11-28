bea196ed05b6e08665659170484f23ee5215096f
David <davidjamescottrell@gmail.com>
2020-11-28 00:21:32 +0000

    Added Staff

--- src/Pages/Home/Staff.jsx
+++ src/Pages/Home/Staff.jsx
@@ -66,7 +66,7 @@ class Staff extends Component {
                 <div className="staff-container">
                     <Employee name="Ellis Wells" img={require("../../Images/EllisWells.png")} statement="I have completed a Post Experience Diploma in Psychology. I have a passion to work with children with additional needs and understand that their environment, interactions and attachment with others has a big impact on their learning." />
                     <Employee name="Sally Wells" img={require("../../Images/SallyWells.png")} statement="I am a Qualified Learning Support Assistant, experienced in SEN Learning Support. I have the patience to support children in their understanding and scaffold their learning." />
-                    {/* <Employee name="Amy Wells" img={require("../../Images/AmyWells.jpg")} statement="I have completed a Certificate of Higher Education in Psychology. I have a strong understanding of how to communicate with children, especially those with additional needs and am always looking for exciting and enriching ways to support children in relaxation and play." /> */}
+                    <Employee name="Anthony Arnold" img={require("../../Images/AnthonyArnold.jpg")} statement="I am a Qualified Learning Support Assistant, experienced in SEN Learning Support. I use humour, patience and story telling to support children's SEMH needs. I use play and art to support their learning." />
                 </div>
                 <div className="card">
                     <h3>Vacancies</h3>
